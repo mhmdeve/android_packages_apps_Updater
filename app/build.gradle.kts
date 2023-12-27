c0585c87b26832b440a661f8b1eeb34469e5bf56
Master <mhmdeveloperofficial@gmail.com>
2023-12-27 12:43:22 +0500

    Rebrand

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.crdroid.updater"
+        applicationId = "org.evolution.ota"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
